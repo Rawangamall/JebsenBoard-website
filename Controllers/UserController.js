90d805c7dc6762d290c83ef3bbdeaeab786ddbe2
Rawangamall <rawan.gamaal21@gmail.com>
2023-08-24 05:26:40 +0300

    validation updates

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -13,20 +13,10 @@ exports.addUser = CatchAsync(async (request, response, next) => {
     const hash = await bcrypt.hash(request.body.password, salt);
 
     const newUser = await User.create({
-      multilingualData: {
-        en: {
-          firstName:request.body.firstName,
-          lastName:request.body.lastName,
-          phoneNumber:request.body.phoneNumber ,
-          role:request.body.role
-        },
-        ar: {
-          firstName:request.body.firstName_ar,
-          lastName:request.body.lastName_ar,
-          phoneNumber:request.body.phoneNumber_ar ,
-          role:request.body.role_ar
-        },
-      },
+      firstName:request.body.firstName,
+      lastName:request.body.lastName,
+      phoneNumber:request.body.phoneNumber ,
+      role:request.body.role,
       email: request.body.email,
       password:hash,
       image:"default.jpg"
@@ -45,17 +35,15 @@ exports.addUser = CatchAsync(async (request, response, next) => {
   
       const whereClause = searchKey ? {
         [Op.or]: [
-          { 'multilingualData.en.firstName': { [Op.like]: `%${searchKey}%` } },
-          { 'multilingualData.en.lastName': { [Op.like]: `%${searchKey}%` } },
-          { 'multilingualData.ar.firstName': { [Op.like]: `%${searchKey}%` } },
-          { 'multilingualData.ar.lastName': { [Op.like]: `%${searchKey}%` } },
+          { firstName: { [Op.like]: `%${searchKey}%` } },
+          { lastName: { [Op.like]: `%${searchKey}%` } },
           { email: { [Op.like]: `%${searchKey}%` } },
         ],
       } : {};
   
       const offset = (page - 1) * limit;
       
-      const attributes = ['id', 'multilingualData', 'email', 'image', 'updatedAt', 'createdAt'];
+      const attributes = ['id', 'firstName' , 'lastName', 'role', 'email','phoneNumber', 'image', 'updatedAt', 'createdAt'];
   
       const users = await User.findAll({
         where: whereClause,
@@ -63,49 +51,23 @@ exports.addUser = CatchAsync(async (request, response, next) => {
         limit,
         offset,
       });
-  
-      const usersWithTranslations = users.map(user => {
-        const { multilingualData, ...userData } = user.toJSON();
-        const { firstName, lastName, phoneNumber, role } = lang === 'en' ? multilingualData.en : multilingualData.ar;
-    
-        return {
-          firstName,
-          lastName,
-          phoneNumber,
-          role,
-          ...userData,
-        };
-      });
 
-      response.status(200).json(usersWithTranslations);
+      response.status(200).json(users);
   }
 );
 
 exports.getUser = CatchAsync(async (request, response, next) => {
 
   const id = request.params._id;
-  const lang = request.headers.lang || "ar";
 
-  const attributes = ['id', 'multilingualData', 'email', 'image', 'updatedAt', 'createdAt'];
-  const data = await User.findByPk(id, { attributes });
+  const attributes = ['id', 'firstName' , 'lastName', 'role', 'email','phoneNumber', 'image', 'updatedAt', 'createdAt'];
+  const user = await User.findByPk(id, { attributes });
 
-  if(!data){
+  if(!user){
     return next(new AppError(`User not found`, 401));
   }
 
-  const { multilingualData, ...userData } = data.toJSON();
-  const { firstName, lastName, phoneNumber, role } = lang === 'en' ? multilingualData.en : multilingualData.ar;
-  
-  const user = {
-    ...userData,
-    firstName,
-    lastName,
-    phoneNumber,
-    role,
-  };
-
-  response.status(200).json(user);
-     
+  response.status(200).json(user);     
   });
 
   exports.editUser = CatchAsync(async (request, response, next) => {
@@ -124,20 +86,16 @@ exports.getUser = CatchAsync(async (request, response, next) => {
     }
 
     const Updateduser = await user.update({
-      'multilingualData.en.firstName': request.body.firstName,
-      'multilingualData.ar.firstName.ar': request.body.firstName_ar,
-      'multilingualData.en.lastName': request.body.lastName,
-      'multilingualData.ar.lastName': request.body.lastName_ar,
+      firstName:request.body.firstName,
+      lastName:request.body.lastName,
+      phoneNumber:request.body.phoneNumber ,
+      role:request.body.role,
       email: request.body.email,
+    //  password:hash,
       password: request.body.password,
-      'multilingualData.en.phoneNumber': request.body.phoneNumber,
-      'multilingualData.ar.phoneNumber': request.body.phoneNumber_ar,
-      'multilingualData.en.role': request.body.role,
-      'multilingualData.ar.role': request.body.role_ar,
       image : request.imageName
     });
 
-    
     response.status(200).json(Updateduser);
   });
   
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -4,17 +4,18 @@ const router=express.Router();
 const userController=require("./../Controllers/UserController");
 const validationData = require("./../Core/Validations/User")
 const imageData = require("./../Core/Validations/imageValidations")
+const validationMW = require("./../Core/Validations/validateMW")
 
 // const AuthenticationMW = require("./../Middlewares/authenticationMW")
 // const AuthorizationMW = require("./../Middlewares/authorizationMW")
 
 router.route("/users")
       .get(userController.getAllUsers) //AuthenticationMW.auth,
-      .post(validationData.UserValidPOST,userController.addUser) //AuthenticationMW.auth,AuthorizationMW.authorize("admin"),
+      .post(validationData.UserValidPOST,validationMW,userController.addUser) //AuthenticationMW.auth,AuthorizationMW.authorize("admin"),
 
 router.route("/user/:_id")
       .get(userController.getUser)  //AuthenticationMW.auth,AuthorizationMW.authorize("admin"),
-      .patch(validationData.UserValidPATCH,imageData.addIMG,userController.editUser) //AuthenticationMW.auth,AuthorizationMW.authorize("admin"),
+      .patch(validationData.UserValidPATCH,validationMW,imageData.addIMG,userController.editUser) //AuthenticationMW.auth,AuthorizationMW.authorize("admin"),
       .delete(userController.delUser) //AuthenticationMW.auth,AuthorizationMW.authorize("admin"),
 
 module.exports=router;
